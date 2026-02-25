9ae93a695d9eee23952e83e6eb7f1272aceacb85
oom <schooltechx@gmail.com>
2026-02-25 23:55:27 +0700

    Add demo

---
+++ deploy/demo/README.md
@@ -0,0 +1,12 @@
+
+
+เพิ่มบรรทัดนี้ใน /etc/hosts
+```
+192.168.2.49 ms-mono-typeorm.local
+```
+ตัวอย่างโปรแกรม ให้สำเนาโฟลเดอร์ demo ไปใช้งาน
+```bash
+cd demo
+mkdir data
+docker compose up -d
+```
--- packages/database/src/data-source.ts
+++ packages/database/src/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import { User, Product, Order } from "@ms-mono-share/database-entities";
 import 'dotenv/config';
 
-const databasePath = process.env.DB_DATABASE || "/data/ms_db.sqlite";
+const databasePath = process.env.DB_DATABASE || "/data/production.sqlite";
 console.log(`Using database path: ${databasePath}`);
 export const AppDataSource = new DataSource({
   type: "sqlite",
